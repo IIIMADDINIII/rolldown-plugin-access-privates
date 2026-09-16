755b05912096dabfc909632b4f05e56698abdc7e
IIIMADDINIII <martin@elend.name>
2026-09-16 21:35:45 +0200

    fix problems with silent errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,33 @@
 import { defineConfig } from "vite-plus";
 
-export default defineConfig({
-  pack: {
-    dts: true,
-    exports: true,
-  },
-  lint: {
-    ignorePatterns: ["/mise/"],
-    options: {
-      typeAware: true,
-      typeCheck: true,
+export default defineConfig((_env) => {
+  return {
+    pack: {
+      entry: ["./src/index.ts"],
+      dts: true,
+      exports: true,
+      platform: "node",
+      format: ["esm"],
+      failOnWarn: true,
     },
-  },
-  fmt: {
-    ignorePatterns: ["/mise/"],
-    sortImports: true,
-    printWidth: 300,
-    jsdoc: {
-      descriptionWithDot: true,
-      lineWrappingStyle: "balance",
+    lint: {
+      jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
+      rules: { "vite-plus/prefer-vite-plus-imports": "error" },
+      ignorePatterns: ["/**/mise/", "/**/declarations.d.ts"],
+      options: {
+        typeAware: true,
+        typeCheck: true,
+        denyWarnings: true,
+      },
     },
-  },
+    fmt: {
+      ignorePatterns: ["/mise/"],
+      sortImports: true,
+      printWidth: 300,
+      jsdoc: {
+        descriptionWithDot: true,
+        lineWrappingStyle: "balance",
+      },
+    },
+  };
 });
